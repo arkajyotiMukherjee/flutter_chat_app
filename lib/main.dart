3f0db566958a571ee273347164edade661904b46
arkajyotiMukherjee <arkajyoti31@gmail.com>
2020-04-30 16:10:10 +0530

    inital commit, added chats streams and channels.

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
+void main() async {
+  final client = Client(
+    'b67pax5b2wdq',
+    logLevel: Level.INFO,
+  );
+
+  await client.setUser(
+    User(id: 'falling-mountain-7'),
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZmFsbGluZy1tb3VudGFpbi03In0.AKgRXHMQQMz6vJAKszXdY8zMFfsAgkoUeZHlI-Szz9E',
+  );
+
+  runApp(MyApp(client));
+}
+
+class MyApp extends StatelessWidget {
+  final Client client;
+
+  MyApp(this.client);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = ThemeData(
+      primarySwatch: Colors.green,
+    );
+
+    return MaterialApp(
+      theme: theme,
+      home: Container(
+        child: StreamChat(
+          streamChatThemeData: StreamChatThemeData.fromTheme(theme).copyWith(
+            ownMessageTheme: MessageTheme(
+              messageBackgroundColor: Colors.black,
+              messageText: TextStyle(
+                color: Colors.white,
+              ),
+              avatarTheme: AvatarTheme(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+          client: client,
+          child: ChannelListPage(),
+        ),
+      ),
+    );
+  }
+}
+
+class ChannelListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChannelListView(
+        filter: {
+          'members': {
+            '\$in': [StreamChat.of(context).user.id],
+          }
+        },
+        sort: [SortOption('last_message_at')],
+        pagination: PaginationParams(
+          limit: 20,
+        ),
+        channelWidget: ChannelPage(),
+      ),
+    );
+  }
+}
+
+class ChannelPage extends StatelessWidget {
+  const ChannelPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ChannelHeader(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: MessageListView(
+              threadBuilder: (_, parentMessage) {
+                return ThreadPage(
+                  parent: parentMessage,
+                );
+              },
+            ),
+          ),
+          MessageInput(),
+        ],
+      ),
+    );
+  }
+}
+
+class ThreadPage extends StatelessWidget {
+  final Message parent;
+
+  ThreadPage({
+    Key key,
+    this.parent,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ThreadHeader(
+        parent: parent,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: MessageListView(
+              parentMessage: parent,
+            ),
+          ),
+          MessageInput(
+            parentMessage: parent,
+          ),
+        ],
+      ),
+    );
+  }
+}
